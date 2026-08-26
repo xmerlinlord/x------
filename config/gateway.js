387ade7450ea55b55ebb8960d6f842a0622622ce
X ᴍᴇʀʟɪɴ ʟᴏʀᴅ <xmerlinlordmdbot@gmail.com>
2026-08-26 17:35:14 +0100

    Create gateway.js

---
+++ config/gateway.js
@@ -0,0 +1,31 @@
+module.exports = {
+  channels: [
+    {
+      name: "Mʀ Dᴀʀᴋ Kɪɴɢ Cʜᴀɴɴᴇʟ",
+      username: "@missarcond",
+      link: "https://t.me/missarcond",
+      type: "channel"
+    },
+    {
+      name: "SMM Pʀᴏᴏғ Cʜᴀɴɴᴇʟ",
+      username: "@babyupdategc",
+      link: "https://t.me/babyupdategc",
+      type: "channel"
+    }
+  ],
+
+  groups: [
+    {
+      name: "Mɪss Tʏʟᴀ Tᴇᴄʜ Gʀᴏᴜᴘ",
+      username: "@missarcond1",
+      link: "https://t.me/missarcond1",
+      type: "group"
+    },
+    {
+      name: "Mɪss Tʏʟᴀ Tᴇᴄʜ",
+      username: "@simontech2027",
+      link: "https://t.me/simontech2027",
+      type: "group"
+    }
+  ]
+};
